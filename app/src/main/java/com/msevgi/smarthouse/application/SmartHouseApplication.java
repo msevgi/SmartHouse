2b62bce9de7268b62a94c13b8b56da8849892a3b
Emre Akturk <emreaktrk@hotmail.com>
2014-12-03 21:32:52 +0200

    Application class added.

---
+++ app/src/main/java/com/msevgi/smarthouse/application/SmartHouseApplication.java
@@ -0,0 +1,11 @@
+package com.msevgi.smarthouse.application;
+
+import android.app.Application;
+
+public class SmartHouseApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
