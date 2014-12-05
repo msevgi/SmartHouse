9a81cdeea47c14e0f2abf14a962f161256540cab
akturke <akturke@netas.com.tr>
2014-12-05 08:48:39 +0200

    Sender ID Changed.

--- app/src/main/java/com/msevgi/smarthouse/constant/ApplicationConstants.java
+++ app/src/main/java/com/msevgi/smarthouse/constant/ApplicationConstants.java
@@ -2,5 +2,5 @@ package com.msevgi.smarthouse.constant;
 
 public final class ApplicationConstants {
 
-    public static final String PROJECT_NUMBER = "43979242575";
+    public static final String PROJECT_NUMBER = "145243954863";
 }
